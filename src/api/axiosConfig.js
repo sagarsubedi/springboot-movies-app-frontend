f18750703d3c32f47a2abb29a65188a6c4c52ee8
Sagar Subedi <sagar@example.com>
2023-01-22 17:16:31 -0600

    fix: cors issue

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://springbootmoviebackend.sagarsubedi.com"
+  baseURL: "https://springbootmoviebackend.sagarsubedi.com",
+  headers: {
+    // 'Access-Control-Allow-Origin': 'https://springbootmoviefrontend.sagarsubedi.com/',
+    'Access-Control-Allow-Origin': 'http://localhost:3000',
+  }
   
 });
