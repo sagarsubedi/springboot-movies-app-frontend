7edb3bf67818f549f7f6de351d09161b448df946
Sagar Subedi <sagar@example.com>
2023-01-22 17:04:12 -0600

    feat: load movies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
+import api from "./api/axiosConfig";
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [movies, setMovies] = useState();
+  const getMovies = async () => { 
+    try {
+      const response = await api.get('/api/v1/movies');
+      setMovies(response.data);
+      console.log(response.data);
+    }
+    catch (error) {
+      console.log(error);
+    }
+
+  }
+  useEffect(() => {
+    getMovies();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
