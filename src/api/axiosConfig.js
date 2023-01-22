e1c08ebee8dba5a4acf99e11d7df32a882be9d8e
Sagar Subedi <sagar@example.com>
2023-01-22 17:29:21 -0600

    config: change baseURL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://springbootmoviebackend.sagarsubedi.com",
+  // baseURL: "https://springbootmoviebackend.sagarsubedi.com",
+  baseURL: "http://localhost:8080",
   headers: {
     // 'Access-Control-Allow-Origin': 'https://springbootmoviefrontend.sagarsubedi.com/',
     'Access-Control-Allow-Origin': 'http://localhost:3000',
