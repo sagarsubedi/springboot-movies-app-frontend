3e353c9de6c9d694d1cedae98107c4eb2ec608f1
Sagar Subedi <sagar@example.com>
2023-01-22 18:03:19 -0600

    refactor: change header for prod

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -4,8 +4,8 @@ export default axios.create({
   baseURL: "https://springbootmoviebackend.sagarsubedi.com",
   // baseURL: "http://localhost:8080",
   headers: {
-    // 'Access-Control-Allow-Origin': 'https://springbootmoviefrontend.sagarsubedi.com/',
-    'Access-Control-Allow-Origin': 'http://localhost:3000',
+    'Access-Control-Allow-Origin': 'https://springbootmoviefrontend.sagarsubedi.com/',
+    // 'Access-Control-Allow-Origin': 'http://localhost:3000',
   }
   
 });
