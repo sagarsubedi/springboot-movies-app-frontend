befa26e703cb8d748f64e5b18a6dac4dd67626ef
Sagar Subedi <sagar@example.com>
2023-01-22 18:02:16 -0600

    refactor: base url to prod

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://springbootmoviebackend.sagarsubedi.com",
-  baseURL: "http://localhost:8080",
+  baseURL: "https://springbootmoviebackend.sagarsubedi.com",
+  // baseURL: "http://localhost:8080",
   headers: {
     // 'Access-Control-Allow-Origin': 'https://springbootmoviefrontend.sagarsubedi.com/',
     'Access-Control-Allow-Origin': 'http://localhost:3000',
